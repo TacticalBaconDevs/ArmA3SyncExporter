4ffbcbb5856c839df25508936104ecf27636e22e
shukari <shukari@users.noreply.github.com>
2022-10-12 22:12:38 +0200

    v1.0.3 (12.10.22) - debugmode; consoleOutput optimized

--- src/main/java/de/tacticalbacon/ExportA3Sync.java
+++ src/main/java/de/tacticalbacon/ExportA3Sync.java
@@ -11,7 +11,9 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardCopyOption;
 import java.nio.file.StandardOpenOption;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,14 +27,20 @@ import fr.soe.a3s.domain.repository.SyncTreeDirectory;
 
 public class ExportA3Sync {
 	
-	public static void main(String[] args) throws IOException {
+	private static boolean DEBUG = false;
+	private static SimpleDateFormat SDF = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+	
+	public static void main(String[] args) {
+		final long startTime = System.currentTimeMillis();
 		File a3sFile = null;
 		
 		try {
-			System.out.println("v1.0.2 (09.10.22)");
+			System.out.println("v1.0.3 (12.10.22)");
 			
 			// parse arguments
 			final Map<String, String> arguments = parseArguments(args);
+			DEBUG = arguments.containsKey("debug");
+			debug("CWD: %s", System.getProperty("user.dir"));
 			
 			// read input
 			final String url = arguments.get("url");
@@ -42,34 +50,56 @@ public class ExportA3Sync {
 				exit("Need a parameter: url to sync or events file", true);
 			
 			// download file
+			debug("Starting download of: %s", url);
 			a3sFile = downloadFile(url);
+			debug("Download of: '%s' finished!", a3sFile.getName());
 			
 			// choose modi
+			debug("Starting processing...");
 			final List<String> ausgabe = new ArrayList<>();
 			if (url.endsWith(DataAccessConstants.SYNC_FILE_NAME)) {
+				debug("Processing: sync");
 				final SyncTreeDirectory sync = (SyncTreeDirectory) A3SFilesAccessor.read(a3sFile);
+				debug("Processing: sync -> read done");
 				final Repo repo = RepoManager.getRepo(sync.getList());
+				debug("Processing: sync -> getRepo done");
 				ausgabe.addAll(RepoManager.csvRepo(repo));
+				debug("Processing: sync -> csvRepo done");
 			} else if (url.endsWith(DataAccessConstants.EVENTS_FILE_NAME)) {
+				debug("Processing: events");
 				final Events events = (Events) A3SFilesAccessor.read(a3sFile);
+				debug("Processing: events -> read done");
 				ausgabe.addAll(RepoManager.csvEvents(events.getList()));
+				debug("Processing: events -> csvEvents done");
 			}
+			debug("Process finished: count: %s | length: %s", ausgabe.size(), ausgabe.stream().mapToInt(String::length).sum());
 			
 			// output modi
+			debug("Starting output...");
 			if (arguments.containsKey("console")) {
+				debug("Output: console");
 				consoleOutput(ausgabe);
+				debug("Output: console ENDE");
 			} else {
 				// write file output
+				debug("Output: file");
 				final String fileName = a3sFile.getName() + ".csv";
 				Files.write(Paths.get(fileName), ausgabe, StandardOpenOption.CREATE);
 				System.out.println(String.format("Output to file: %s", fileName));
 			}
+			debug("Output finished!");
 		} catch (final Exception e) {
 			exit("Fehler: %s\r\n%s", true, e.getLocalizedMessage(), getStacktrace(e));
 		} finally {
 			// remove temp downloaded files
 			if (a3sFile != null)
-				Files.deleteIfExists(a3sFile.toPath());
+				try {
+					Files.deleteIfExists(a3sFile.toPath());
+				} catch (final IOException e) {
+					exit("Fehler2: %s\r\n%s", true, e.getLocalizedMessage(), getStacktrace(e));
+				}
+			
+			debug("Execution time: %sms", System.currentTimeMillis() - startTime);
 		}
 	}
 	
@@ -83,6 +113,11 @@ public class ExportA3Sync {
 			help();
 		
 		exit(msg, args);
+	}
+	
+	private static void debug(String msg, Object... args) {
+		if (DEBUG)
+			System.out.println(String.format("[DEBUG][%s] %s", SDF.format(new Date()), String.format(msg, args)));
 	}
 	
 	private static Map<String, String> parseArguments(String[] args) {
@@ -107,6 +142,7 @@ public class ExportA3Sync {
 				+ "Options:\r\n"
 				+ "\t-h / -? / -help: shows help\r\n"
 				+ "\t-console: output option as console output\r\n"
+				+ "\t-debug: more informations\r\n"
 				+ "URL:\r\n"
 				+ "\thttp://***/sync => ADDON|FILE1|FILE2|...\r\n"
 				+ "\thttp://***/events => EVENT|ADDON1|ADDON2|..."));
@@ -133,7 +169,10 @@ public class ExportA3Sync {
 	}
 	
 	private static void consoleOutput(List<String> ausgabe) {
-		System.out.println(String.format("-----------------START-----------------\r\n%s\r\n-----------------END-----------------", String.join("\r\n", ausgabe)));
+		System.out.println("-----------------START-----------------");
+		for (final String line : ausgabe)
+			System.out.println(line);
+		System.out.println("-----------------END-----------------");
 	}
 	
 	private static String getStacktrace(Exception e) {
